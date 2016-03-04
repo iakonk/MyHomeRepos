7e260af213586aa9222d98056fae701cdb742323
root <konkina.iana@gmail.com>
2016-03-04 00:15:05 -0800

    removed old data

--- home/managment/commands/tornado_start.py
+++
@@ -1,6 +0,0 @@
-import home.views
-
-define('port', type=int, default=8080)
-
-uwsgi_apps =
-
--- settings/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = '0.1'
-__description__ = """Django project to share latest IT solutions and interesting facts about used technologies
-                from real-life projects"""
