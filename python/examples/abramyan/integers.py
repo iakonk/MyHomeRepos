363f9da2c2d055f9505d0cb8dcd315a67ef69ba4
iakonk <konkina.iana@gmail.com>
2020-02-17 22:52:18 +0200

    integers added

--- python/examples/abramyan/integers.py
+++ python/examples/abramyan/integers.py
@@ -72,3 +72,119 @@ def find_units_and_dozens(num):
 
 
 assert find_units_and_dozens(123) == (3, 2)
+
+
+def integer_11(num):
+    """ 100 <= num < 1000 """
+    hundreds = num // 100
+    dozens = (num % 100) // 10
+    units = (num % 100) % 10
+    return hundreds + dozens + units, hundreds * dozens * units
+
+
+assert integer_11(100) == (1, 0)
+assert integer_11(350) == (8, 0)
+
+
+def integer_12(num):
+    """ 100 <= num < 1000 """
+    hundreds = num // 100
+    dozens = (num % 100) // 10
+    units = (num % 100) % 10
+    return int('%s%s%s' % (units, dozens, hundreds))
+
+
+assert integer_12(300) == 3
+assert integer_12(524) == 425
+
+
+def integer_13(num):
+    """ 100 <= num < 1000 """
+    hundreds, dozens = num // 100, num % 100
+    return int('%s%s' % (dozens, hundreds))
+
+
+assert integer_13(534) == 345
+
+
+def integer_14(num):
+    """ 100 <= num < 1000 """
+    hundreds, dozens, units = num // 100,  num % 100, num % 100 % 10
+    return int('%s%s%s' % (units, hundreds, dozens //10))
+
+
+assert integer_14(456) == 645, print(integer_14(456))
+
+
+def integer_15(num):
+    """ 100 <= num < 1000 """
+    first_digit = num // 100
+    sec_digit = (num % 100) // 10
+    third_digit = num % 100 % 10
+    return int('%s%s%s' % (sec_digit, first_digit, third_digit))
+
+
+assert integer_15(123) == 213
+
+
+def integer_16(num):
+    """ 100 <= num < 1000 """
+    first_digit = num // 100
+    sec_digit = (num % 100) // 10
+    third_digit = num % 100 % 10
+    return int('%s%s%s' % (first_digit, third_digit, sec_digit))
+
+
+assert integer_16(123) == 132
+
+
+def integer_17(num):
+    """ num > 999 """
+    return num % 1000 / 100
+
+
+assert integer_17(1300) == 3
+assert integer_17(20200) == 2
+
+
+def integer_18(num):
+    """ num > 999 """
+    pass
+
+
+def integer_19(sec):
+    ONE_MIN = 60
+    return sec // ONE_MIN
+
+
+assert integer_19(3600) == 60
+assert integer_19(3605) == 60
+
+
+def integer_20(num):
+    ONE_MIN, ONE_HOUR = 60, 60
+    return num // ONE_MIN // ONE_HOUR
+
+
+assert integer_20(3625) == 1
+
+
+def integer_21(num):
+    """ seconds since last minute """
+    ONE_MIN = 60
+    return num % ONE_MIN
+
+
+assert integer_21(61) == 1
+assert integer_21(125) == 5
+
+
+def integer_22(num):
+    """ seconds since last hour """
+    ONE_MIN, ONE_HOUR = 60, 60
+    pass
+
+
+def integer_23(num):
+    """ minutes since last hour"""
+    pass
